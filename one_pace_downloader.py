42546e511132878b85c5871db483826cd33512d2
zayedAlmansoori <zayedsaifalmansoori@gmail.com>
2023-09-23 03:49:41 +0400

    Add Page class Add Arc class Add get_arc function to Page: returns Arc instance

--- one_pace_downloader.py
+++ one_pace_downloader.py
@@ -1,5 +1,6 @@
 import requests
 import logging
+from bs4 import BeautifulSoup
 
 # Creating Logger
 logging.basicConfig(filename="one_pace_downloader.log", format='%(asctime)s %(message)s', encoding='utf-8',
@@ -11,24 +12,60 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+class Arc:
+    def __init__(self, name, links):
+        self.name = name
+        self.episodes = self.get_magnet_links(links)
 
-def get_download_page():
-    url = "https://onepace.net/watch"
-    try:
-        resp = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0"})
-        status = resp.status_code
-        if status == 200:
-            pass
+    def get_magnet_links(self, links):
+        magnets = [link for link in links if 'magnet' in link]
+        print(magnets)
+        return magnets
+
+    def __repr__(self):
+        return self.name
+
+class Page:
+    def __init__(self):
+        self.page = self.get_download_page()
+
+    def get_download_page(self):
+        url = "https://onepace.net/watch"
+        try:
+            resp = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0"})
+            status = resp.status_code
+            if status == 200:
+                pass
+            else:
+                raise ConnectionError("Request failed")
+        except ConnectionError as err:
+            logger.info(err)
         else:
-            raise ConnectionError("Request failed")
-    except ConnectionError as err:
-        logger.info(err)
-    else:
-        return resp.text
+            bs = BeautifulSoup(resp.content, 'html.parser')
+            return bs
+
+    def get_arc(self, arc):
+        name = None
+        links = None
+        try:
+            for section in self.page.find_all('section', attrs={"class": "Carousel_root__t7h0u"}):
+                if section.find('h2').text.lower() == arc:
+                    name = section.find('h2').text
+                    links = [a['href'] for a in section.find_all('div', href=True)]
+                    break
+            if not name or not links:
+                raise TypeError("Failed to find Arc")
+        except TypeError as err:
+            logger.info(err)
+        else:
+            arc = Arc(name, links)
+            return arc
+
 
 
 def main():
-    pass
+    page = Page()
+
 
 if __name__ == '__main__':
     main()
--- test_one_pace_downloader.py
+++ test_one_pace_downloader.py
@@ -1,8 +1,22 @@
 import unittest
 import one_pace_downloader
+from bs4 import BeautifulSoup
 
 class TestOnePaceDownloader(unittest.TestCase):
     # test if get_download_page retreives the page
     def test_get_download_page(self):
-        self.assertIsInstance(one_pace_downloader.get_download_page(), str)
-    def test_if_exception_is_raised_on_unsuccessful_reqeust(self):
+        p = one_pace_downloader.Page()
+        self.assertEqual(p.page.find('title').text, 'Watch One Pace | The Definitive One Piece Viewing Experience')
+
+    def test_get_arc_by_title(self):
+        arc = 'water seven'
+        p = one_pace_downloader.Page()
+
+        self.assertEqual((p.get_arc(arc)).name.lower(), "water seven")
+    def test_if_arc_class_gets_magnet_links(self):
+        arc = "water seven"
+        p = one_pace_downloader.Page()
+
+        arc = p.get_arc(arc)
+
+        self.assertIsInstance(arc.episodes, list)
